94ab09c83660ea5674fd3b5a3f8c1308fae32c66
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-09-18 10:04:25 -0600

    oops missed diane_arbus variable holding the artist object to pass in

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -150,7 +150,7 @@ class CuratorTest<Minitest::Test
 
    curator.add_artist(artist_1)
    curator.add_artist(artist_2)
-   #does this need to be returned outside the array?
+
    assert_equal artist_1, curator.find_artist_by_id("1")
   end
 
@@ -177,6 +177,83 @@ class CuratorTest<Minitest::Test
     curator.add_photograph(photo_2)
 
     assert_equal photo_2, curator.find_photograph_by_id("2")
+  end
+
+  def test_i_can_find_photographs_by_artist
+    curator = Curator.new
+
+    photo_1_attributes = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+
+    photo_2_attributes = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+
+    photo_3_attributes = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    }
+
+    photo_4_attributes = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    }
+
+    artist_1_attributes = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+
+   artist_2_attributes = {
+     id: "2",
+     name: "Ansel Adams",
+     born: "1902",
+     died: "1984",
+     country: "United States"
+   }
+
+   artist_3_attributes = {
+     id: "3",
+     name: "Diane Arbus",
+     born: "1923",
+     died: "1971",
+     country: "United States"
+   }
+
+   photo_1 = Photograph.new(photo_1_attributes)
+   photo_2 = Photograph.new(photo_2_attributes)
+   photo_3 = Photograph.new(photo_3_attributes)
+   photo_4 = Photograph.new(photo_4_attributes)
+
+   artist_1 = Artist.new(artist_1_attributes)
+   artist_2 = Artist.new(artist_2_attributes)
+   artist_3 = Artist.new(artist_3_attributes)
+
+   curator.add_photograph(photo_1)
+   curator.add_photograph(photo_2)
+   curator.add_photograph(photo_3)
+   curator.add_photograph(photo_4)
+
+   curator.add_artist(artist_1)
+   curator.add_artist(artist_2)
+   curator.add_artist(artist_3)
+
+   diane_arbus = curator.find_artist_by_id("3")
+   
   end
 
 end
